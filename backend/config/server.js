fe6fdc1618a1e82ef2ee97c85c20b2b773198e7e
pietarin <pietari.pakarinen@tuni.fi>
2020-07-29 12:30:21 +0300

    project setup

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '98b3093137c723f7ef612a0b8ecee302'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '8a46991b-fd5a-4db7-bd1e-b7663f90b315'
+};
